9f8851080be934dad25d8d0e7ae4c543dfee8b43
shresth raj <shresthraj1012001@gmail.com>
2026-05-11 21:55:34 +0530

    intialize ordercontroller

--- src/main/java/com/project/foms/controller/OrderController.java
+++ src/main/java/com/project/foms/controller/OrderController.java
@@ -3,8 +3,16 @@ package com.project.foms.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.project.foms.service.orderService.OrderService;
+
 @RestController
 @RequestMapping("/order")
 public class OrderController {
+    
+    private final OrderService orderService;
+    public OrderController(OrderService orderService){
+        this.orderService = orderService;
+    }
+
     
 }
