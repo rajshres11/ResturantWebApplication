f0369a5ebb4578683d62f9ee93a6f2004de3e45d
shresth raj <shresthraj1012001@gmail.com>
2026-05-13 01:22:37 +0530

    added placeorder feature

--- src/main/java/com/project/foms/controller/OrderController.java
+++ src/main/java/com/project/foms/controller/OrderController.java
@@ -1,18 +1,34 @@
 package com.project.foms.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.project.foms.dto.ApiResponse;
+import com.project.foms.dto.orderdto.OrderRequestDto;
+import com.project.foms.dto.orderdto.OrderResponseDto;
 import com.project.foms.service.orderService.OrderService;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
 @RequestMapping("/order")
 public class OrderController {
+    
     
     private final OrderService orderService;
     public OrderController(OrderService orderService){
         this.orderService = orderService;
     }
 
+    @PostMapping("/save")
+    public ResponseEntity<ApiResponse<OrderResponseDto>> placeOrder(@RequestBody OrderRequestDto o){
+        OrderResponseDto order = orderService.placeOrder(o);
+        ApiResponse<OrderResponseDto> response = new ApiResponse<>(HttpStatus.CREATED.value(),"Order created sussesfully",order);
+        return new ResponseEntity<>(response,HttpStatus.CREATED);
+    }
+    
     
 }
